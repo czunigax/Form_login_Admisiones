d3f52e38757fdc0e11ecd9828a67594556440ac1
czunigac99 <czunigac2@outlook.com>
2023-11-02 20:46:11 -0600

    Funcion que verifica validez del csv

--- src/routes/Registrar_Notas.tsx
+++ src/routes/Registrar_Notas.tsx
@@ -10,22 +10,53 @@ interface CSVData {
 export default function Registrar_Notas (){
 
     const [tableData, setTableData] = useState<CSVData[]>([]);
+   // const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
+
+   const isCSVValid = (csvData: string): boolean => {
+    // Dividir el CSV en filas
+    const rows = csvData.split(/\r\n|\n|\r/);
+  
+    // Validar que todas las filas contienen tres columnas
+    for (const row of rows) {
+      const columns = row.split(",");
+      if (columns.length == 3) {
+        return true; // Si alguna fila no tiene tres columnas, el formato no es válido
+      }
+    }
+  
+    // Si todas las filas tienen tres columnas, el formato es válido
+    return false;
+  };
+
+    
     const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files && event.target.files[0];
-        if (file) {
-          const reader = new FileReader();
-      
-          reader.onload = (e) => {
-            if (e.target) {
-              const csvData = e.target.result as string;
+      const file = event.target.files && event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+    
+        reader.onload = (e) => {
+          if (e.target) {
+            const csvData = e.target.result as string;
+            
+            // Verificar que el archivo CSV tenga tres columnas
+            if (!isCSVValid(csvData)) {
+              // Si el archivo CSV no es válido, no deberia procesar
+              alert("El archivo CSV no tiene el formato correcto (tres columnas).");
+              return; 
+            } else {
+              // Si el archivo CSV  es válido, procesa
               parseCSVData(csvData);
             }
-          };
-      
-          reader.readAsText(file);
-        }
-      };
+          }
+        };
+    
+        reader.readAsText(file);
+      }
+    };
+    
+ 
+
 
 
             const parseCSVData = (csvData: string) => {
